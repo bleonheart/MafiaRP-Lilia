679d711b96e9bb8c7548bee0c868510871c17dff
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-09-06 10:57:42 +0100

    Updated for 2.0

--- README.md
+++ README.md
@@ -4,10 +4,12 @@ MafiaRP is a roleplay schema for [Lilia 1.0](https://github.com/bleonheart/Lilia
 
 ## Getting Started
 
-To get started, you should look at the core folder, located inside of the [Schema Folder](https://github.com/bleonheart/MafiaRP-Lilia/tree/main/gamemodes/mafiarp/schema) and configure the libraries using your own settings. After such, you can start configuring the plugins to your liking.
+To get started, you should look at the core folder, located inside of the [Schema Folder](https://github.com/bleonheart/MafiaRP-Lilia/tree/main/gamemodes/mafiarp/schema) and configure the schema using your own settings. After such, you can start configuring the modules to your liking.
 
 If you have questions regarding the schema or want to integrate with the community, you can join our Discord server at [Discord](https://discord.gg/RTcVq92HsH).
 
-## Collection
+## Content
 
-https://steamcommunity.com/sharedfiles/filedetails/?id=2833154367
+For the StunGun:
+
+https://steamcommunity.com/sharedfiles/filedetails/?id=428174344
---
+++ gamemodes/mafiarp/schema/sh_config.lua
@@ -0,0 +1,26 @@
+--------------------------------------------------------------------------------------------------------
+lia.config.version = "1.0"
+lia.config.DefaultGamemodeName = "MafiaRP"
+--------------------------------------------------------------------------------------------------------
+lia.currency.singular = "Dollar"
+lia.currency.plural = "Dollars"
+lia.currency.symbol = "$"
+--------------------------------------------------------------------------------------------------------
+lia.config.MoneyModel = "models/props_lab/box01a.mdl"
+lia.config.MaxCharacters = 1
+lia.config.invW = 12
+lia.config.invH = 4
+lia.config.WalkSpeed = 130
+lia.config.RunSpeed = 235
+lia.config.WalkRatio = 0.5
+lia.config.DefaultMoney = 0 
+--------------------------------------------------------------------------------------------------------
+lia.config.Color = Color(75, 119, 190) // Schema Color Code
+lia.config.VersionEnabled = true // Version on Bottom Left?
+lia.config.DarkTheme = true // Dark Mode?
+lia.config.AutoWorkshopDownloader = true // Automatically Download Workshop?
+--------------------------------------------------------------------------------------------------------
+lia.config.DefaultStaff = {
+    ["STEAM_0:1:176123778"] = "superadmin", -- Leonheart // PLEASE CHANGE ME
+}
+--------------------------------------------------------------------------------------------------------
