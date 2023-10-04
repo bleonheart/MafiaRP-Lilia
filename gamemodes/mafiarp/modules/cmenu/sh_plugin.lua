37ba3d403c23aabee4cbd1e7271acde9785600d1
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-10-04 12:24:27 +0100

    Added Self Downloading Workshop

--- gamemodes/mafiarp/modules/cmenu/sh_plugin.lua
+++
@@ -1,4 +0,0 @@
-MODULE.name = "Context Menu Improves"
-MODULE.author = "STEAM_0:1:176123778"
-MODULE.desc = "Adds Several Shortcuts to CMenu."
-lia.util.include("cl_plugin.lua")
--- gamemodes/mafiarp/modules/tying/sh_plugin.lua
+++
@@ -1,5 +0,0 @@
-MODULE.name = "Tying"
-MODULE.author = "STEAM_0:1:176123778"
-MODULE.desc = "Adds the ability to Tie Players, Drag Them, Gag Them and BlindFold them."
-lia.util.include("sv_plugin.lua")
-lia.util.include("cl_plugin.lua")
