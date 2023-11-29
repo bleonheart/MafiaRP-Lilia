a03ac10d110b3bfd15d794e4a3ac025e013d50a4
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-11-29 23:48:41 +0100

    Removed PIM

--- mafiarp/modules/pim/sv_module.lua
+++
@@ -1,7 +0,0 @@
-----------------------------------------------------------------------------------------------
-local netcalls = {"cmenu", "ApproveSearch", "RequestSearch", "blindfold", "gag_player", "vehicle_usage", "cmenu_tying", "FF", "RequestID", "ApproveID"}
-----------------------------------------------------------------------------------------------
-for k, v in pairs(netcalls) do
-    util.AddNetworkString(v)
-end
-----------------------------------------------------------------------------------------------
