31a83954d52cb9692677a2a476bd1ce2ce559b0f
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-05-23 19:58:09 +0100

    Optimized Several Things

--- mafiarp/modules/stungun/meta/server.lua
+++ mafiarp/modules/stungun/meta/server.lua
@@ -78,7 +78,7 @@ function playerMeta:setRagdolledTase(state, time)
                     self:SetPos(entity:GetPos())
                     if velocity:Length2D() >= 8 then
                         if not entity.liaPausing then
-                            self:setAction()
+                            self:stopAction()
                             entity.liaPausing = true
                         end
                         return
