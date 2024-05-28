30a5ad5fc20510c4e4dc562365ae6222468a8e1b
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-05-28 10:15:24 +0000

    Added Temporary Values to Docs

--- mafiarp/modules/radio/docs/config.lua
+++ mafiarp/modules/radio/docs/config.lua
@@ -4,4 +4,4 @@
 --- This table defines the default settings for the Protection Module.
 -- @realm shared
 -- @table Configuration
--- @field TempValue Indicates whether Family Sharing is enabled on this server | **bool**
+-- @field TempValue TEMP VALUE | **bool**
