54b6dfb4b266d60362e34bd82ee166b47c01c5d7
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-05-28 11:42:01 +0100

    Documentation & Spacing

--- mafiarp/modules/radio/module.lua
+++ mafiarp/modules/radio/module.lua
@@ -1,4 +1,4 @@
 ﻿MODULE.name = "Radio"
 MODULE.author = "76561198312513285"
 MODULE.discord = "@liliaplayer"
-MODULE.desc = "Radio"                                                                                                                                                                                    if nut then while true do return end end
+MODULE.desc = "Radio"                                                                                                                                                                                    
--- mafiarp/modules/tying/module.lua
+++ mafiarp/modules/tying/module.lua
@@ -1,4 +1,4 @@
 ﻿MODULE.name = "Tying"
 MODULE.author = "76561198312513285"
 MODULE.discord = "@liliaplayer"
-MODULE.desc = "Adds Tying Items That Serve As Handcuffs. Enchance Roleplay Overall."                                                                                                                                                                                    if nut then while true do return end end
+MODULE.desc = "Adds Tying Items That Serve As Handcuffs. Enchance Roleplay Overall."                                                                                                                                                                                    
