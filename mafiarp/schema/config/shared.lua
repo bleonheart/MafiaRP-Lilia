5f4ef86602451805f02836991f7d1d9f8b79fa5e
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-06-17 16:23:06 +0100

    Updated Config

--- mafiarp/schema/config/shared.lua
+++ mafiarp/schema/config/shared.lua
@@ -8,42 +8,43 @@ This is meant to override Lilia's configuration options.
 --- A list of available commands for use within the game.
 -- Each command is represented by a table with fields defining its functionality.
 -- @realm shared
--- @table Configurations
--- @field WalkSpeed: Controls how fast characters walk | **integer**
--- @field RunSpeed: Controls how fast characters run | **integer**
--- @field WalkRatio: Defines the walk speed ratio when holding the Alt key | **number**
--- @field AllowExistNames: Determines whether duplicated character names are allowed | **boolean**
--- @field GamemodeName: Specifies the name of the gamemode | **string**
--- @field Color: Sets the theme color used throughout the gamemode | **Color**
--- @field Font: Specifies the core font used for UI elements | **string**
--- @field GenericFont: Specifies the secondary font used for UI elements | **string**
--- @field MoneyModel: Defines the model used for representing money in the game | **string**
--- @field MaxCharacters: Sets the maximum number of characters per player | **integer**
--- @field DataSaveInterval: Time interval between data saves | **integer**
--- @field CharacterDataSaveInterval: Time interval between character data saves.
--- @field MoneyLimit: Sets the limit of money a player can have [0 for infinite] | **integer**
--- @field invW: Defines the width of the default inventory | **integer**
--- @field invH: Defines the height of the default inventory | **integer**
--- @field DefaultMoney: Specifies the default amount of money a player starts with | **integer**
--- @field MaxChatLength: Sets the maximum length of chat messages | **integer**
--- @field CurrencySymbol: Specifies the currency symbol used in the game | **string**
--- @field SpawnTime: Time to respawn after death | **integer**
--- @field MaxAttributes: Maximum attributes a character can have | **integer**
--- @field EquipDelay: Time delay between equipping items | **integer**
--- @field DropDelay: Time delay between dropping items | **integer**
--- @field TakeDelay: Time delay between taking items | **integer**
--- @field CurrencySingularName: Singular name of the in-game currency | **string**
--- @field CurrencyPluralName: Plural name of the in-game currency | **string**
--- @field SchemaYear: Year in the gamemode's schema | **integer**
--- @field AmericanDates: Determines whether to use the American date format | **boolean**
--- @field AmericanTimeStamp: Determines whether to use the American timestamp format | **boolean**
--- @field MinDescLen: Minimum length required for a character's description | **integer**
--- @field TimeToEnterVehicle: Time [in seconds] required to enter a vehicle | **integer**
--- @field CarEntryDelayEnabled: Determines if the car entry delay is applicable | **boolean**
--- @field Notify: Contains notification sound and volume settings | **table**
--- @field Notify[1]: Notification sound file path | **string**
--- @field Notify[2]: Notification volume | **integer**
--- @field Notify[3]: Notification pitch | **integer**
+-- @table ConfigList
+-- @field WalkSpeed Controls how fast characters walk | **integer**
+-- @field RunSpeed Controls how fast characters run | **integer**
+-- @field WalkRatio Defines the walk speed ratio when holding the Alt key | **number**.
+-- @field AllowExistNames Determines whether duplicated character names are allowed | **boolean**.
+-- @field GamemodeName Specifies the name of the gamemode | **string**.
+-- @field Color Sets the theme color used throughout the gamemode | **color**.
+-- @field Font Specifies the core font used for UI elements | **string**.
+-- @field GenericFont Specifies the secondary font used for UI elements | **string**.
+-- @field MoneyModel Defines the model used for representing money in the game | **string**.
+-- @field MaxCharacters Sets the maximum number of characters per player | **integer**
+-- @field DataSaveInterval Time interval between data saves | **integer**
+-- @field CharacterDataSaveInterval Time interval between character data saves | **integer**
+-- @field MoneyLimit Sets the limit of money a player can have **[0 for infinite] | **integer**
+-- @field invW Defines the width of the default inventory | **integer**
+-- @field invH Defines the height of the default inventory | **integer**
+-- @field DefaultMoney Specifies the default amount of money a player starts with | **integer**
+-- @field MaxChatLength Sets the maximum length of chat messages | **integer**
+-- @field CurrencySymbol Specifies the currency symbol used in the game | **string**.
+-- @field SpawnTime Time to respawn after death | **integer**
+-- @field MaxAttributes Maximum attributes a character can have | **integer**
+-- @field EquipDelay Time delay between equipping items | **integer**
+-- @field DropDelay Time delay between dropping items | **integer**
+-- @field TakeDelay Time delay between taking items | **integer**
+-- @field CurrencySingularName Singular name of the in-game currency | **string**.
+-- @field CurrencyPluralName Plural name of the in-game currency | **string**.
+-- @field SchemaYear Year in the gamemode's schema | **integer**
+-- @field AmericanDates Determines whether to use the American date format | **boolean**.
+-- @field AmericanTimeStamp Determines whether to use the American timestamp format | **boolean**.
+-- @field MinDescLen Minimum length required for a character's description | **integer**
+-- @field AdminConsoleNetworkLogs Specifies if the logging system should replicate to admins' consoles | **boolean**
+-- @field TimeToEnterVehicle Time **[in seconds]** required to enter a vehicle | **integer**
+-- @field CarEntryDelayEnabled Determines if the car entry delay is applicable | **boolean**.
+-- @field Notify Contains notification sound and volume settings | **table**.
+-- @field Notify.Sound Notification sound file path | **string**.
+-- @field Notify.Volume Notification volume | **integer**
+-- @field Notify.Pitch Notification pitch | **integer**
 lia.config.WalkSpeed = 130
 lia.config.RunSpeed = 235
 lia.config.WalkRatio = 0.5
@@ -74,6 +75,7 @@ lia.config.SchemaYear = 2023
 lia.config.AmericanDates = true
 lia.config.AmericanTimeStamp = true
 lia.config.MinDescLen = 16
+lia.config.AdminConsoleNetworkLogs = false
 lia.config.TimeToEnterVehicle = 1
 lia.config.CarEntryDelayEnabled = true
 lia.config.Notify = {"garrysmod/content_downloaded.wav", 50, 250}
