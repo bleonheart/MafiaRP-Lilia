e2c28f1f4bccf713b6417599b1646f59dc062647
Samael <33399712+bleonheart@users.noreply.github.com>
2025-01-13 04:18:21 +0000

    Updated Base Models

--- mafiarp/schema/factions/attorneys.lua
+++ mafiarp/schema/factions/attorneys.lua
@@ -2,5 +2,5 @@
 FACTION.desc = "The Attorneys of New York "
 FACTION.color = Color(255, 255, 255)
 FACTION.isDefault = false
-FACTION.models = {"models/Humans/Group02/male_07.mdl", "models/Humans/Group02/male_07.mdl", "models/Humans/Group02/male_07.mdl", "models/Humans/Group02/male_07.mdl", "models/Humans/Group02/male_07.mdl"}
+FACTION.models = {"models/player/Barney.mdl", "models/player/gman_high.mdl", "models/player/alyx.mdl"}
 FACTION_ATTORNEYS = FACTION.index
